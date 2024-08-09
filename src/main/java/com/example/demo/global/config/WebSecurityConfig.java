687ded5093c49e412540e12e204d6863c4b52ad5
RebbitK <andxl9979@gmail.com>
2024-08-09 18:59:51 +0900

    :recycle: 로그인 기능 필터가 아닌 서비스단에서 하도록 변경

--- src/main/java/com/example/demo/global/config/WebSecurityConfig.java
+++ src/main/java/com/example/demo/global/config/WebSecurityConfig.java
@@ -1,6 +1,5 @@
 package com.example.demo.global.config;
-import com.example.demo.domain.user.repository.UserRepository;
-import com.example.demo.global.security.JwtAuthenticationFilter;
+
 import com.example.demo.global.security.JwtAuthorizationFilter;
 import com.example.demo.global.util.JwtUtil;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -26,9 +25,6 @@ public class WebSecurityConfig {
 
 	private final JwtUtil jwtUtil;
 	private final ObjectMapper objectMapper;
-	private final AuthenticationConfiguration authenticationConfiguration;
-	private final UserRepository userRepository;
-	private final PasswordEncoder passwordEncoder;
 
 	@Bean
 	public PasswordEncoder passwordEncoder() {
@@ -46,13 +42,6 @@ public class WebSecurityConfig {
 		return new JwtAuthorizationFilter(jwtUtil, objectMapper);
 	}
 
-	@Bean
-	public JwtAuthenticationFilter jwtAuthenticationFilter() throws Exception {
-		JwtAuthenticationFilter filter = new JwtAuthenticationFilter(jwtUtil, userRepository,
-			passwordEncoder);
-		filter.setAuthenticationManager(authenticationManager(new AuthenticationConfiguration()));
-		return filter;
-	}
 
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -69,15 +58,15 @@ public class WebSecurityConfig {
 				.requestMatchers(PathRequest.toStaticResources().atCommonLocations())
 				.permitAll()
 				.requestMatchers("/auth/**").permitAll()
-				.requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll()
-				.requestMatchers(HttpMethod.GET,"/health-check").permitAll()
+				.requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html")
+				.permitAll()
+				.requestMatchers(HttpMethod.GET, "/health-check").permitAll()
 				.anyRequest().authenticated()
 
 		);
 
 		// 필터 관리
 		http.addFilterBefore(jwtAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class);
-		http.addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
 
 		return http.build();
 	}
