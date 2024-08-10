d0285fe8581952c55bfaa74ca290403cd01467d3
RebbitK <andxl9979@gmail.com>
2024-08-11 02:05:45 +0900

    :sparkles: controller 테스트 추가

--- src/main/java/com/example/demo/domain/user/dto/LoginRequestDto.java
+++ src/main/java/com/example/demo/domain/user/dto/LoginRequestDto.java
@@ -1,9 +1,11 @@
 package com.example.demo.domain.user.dto;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
 @Setter
 @Getter
+@AllArgsConstructor
 public class LoginRequestDto {
 
 	private String username;
